11fb4b9a74e327850b5bfc848a8e4812b95ca34c
Felix Schulze Sindern <f.schulzesindern@web.de>
2024-09-29 18:36:38 +0200

    Initial commit

---
+++ src/config/footer.js
@@ -0,0 +1,76 @@
+export const footerSocials = [
+	{
+		name: 'Instagram',
+		url: 'https://www.instagram.com/jungschuetzenvereinbarlo_ev_/',
+		icon: 'mdi:instagram',
+	},
+	{
+		name: 'WhatsApp',
+		url: 'https://chat.whatsapp.com/DZwS3bet8879YausotZUCV',
+		icon: "mdi:whatsapp",
+	},
+	{
+		name: 'Facebook',
+		url: 'https://www.facebook.com/JungschuetzenvereinBarlo',
+		icon: "mdi:facebook",
+	},
+	{
+		name: 'Github',
+		url: 'https://github.com/FelixSchuSi/',
+		icon: "mdi:github",
+	}
+]
+
+export const footerLists = [
+	{
+		title: 'Landing Pages',
+		items: [
+			{
+				title: 'Landing Page 1',
+				slug: '/landing-pages/landing-1',
+			},
+			{
+				title: 'Landing Page 2',
+				slug: '/landing-pages/landing-2',
+			}
+		],
+	},
+	{
+		title: 'Company',
+		items: [
+			{
+				title: 'About',
+				slug: '/company/about',
+			},
+			{
+				title: 'Blog',
+				slug: '/blog',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	},
+	{
+		title: 'Theme',
+		items: [
+			{
+				title: 'Get Started',
+				slug: '/theme/get-started',
+			},
+			{
+				title: 'Style Guide',
+				slug: '/theme/style-guide',
+			},
+			{
+				title: 'Theme Setup',
+				slug: '/theme/theme-setup',
+			},
+			{
+				title: 'Customizing Odyssey',
+				slug: '/theme/customizing-odyssey',
+			}
+		],
+	},
+];
---
+++ src/config/settings.js
@@ -0,0 +1,7 @@
+export default {
+	title: `Jungschützenverein Barlo`,
+	description: `Wilkommen auf der Website vom Jungschützenverein Barlo.`,
+	url: `https://jungschuetzenverein-barlo.de`, // No trailing slash!
+	name: `Jungschützenverein Barlo e. V.`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+};
