b9db0acb983e50cbe482aadbd7d6c47a3a996ad1
Felix Schulze Sindern <f.schulzesindern@web.de>
2024-09-30 15:13:52 +0200

    use typescript

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
@@ -9,6 +10,5 @@ import { BASE_PATH_WITHOUT_DOMAIN, SITE } from './base-path.mjs';
 export default defineConfig({
 	site: SITE,
 	base: BASE_PATH_WITHOUT_DOMAIN,
-	sitemap: true, // Generate sitemap (set to "false" to disable)
-	integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+	integrations: [sitemap(), mdx(), lit(), icon()]
 });
