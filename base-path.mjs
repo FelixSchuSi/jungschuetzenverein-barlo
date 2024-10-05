71a77e66e93a0d2a80fac96c0054ce357f4f0fda
Felix Schulze Sindern <47390169+FelixSchuSi@users.noreply.github.com>
2024-10-05 16:04:26 +0200

    Set domain

--- base-path.mjs
+++ base-path.mjs
@@ -1,2 +1,2 @@
-export const BASE_PATH_WITHOUT_DOMAIN = '/jungschuetzenverein-barlo';
-export const SITE = 'https://felixschusi.github.io';
+export const BASE_PATH_WITHOUT_DOMAIN = '';
+export const SITE = 'https://jungschuetzenverein-barlo.de';
