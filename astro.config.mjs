b5ea0d39bc1496e50b7f355ced38d31e571eba9d
Felix Schulze Sindern <f.schulzesindern@web.de>
2024-10-06 18:18:36 +0200

    Implement own image-carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
-import lit from '@astrojs/lit';
 import { BASE_PATH_WITHOUT_DOMAIN, SITE } from './base-path.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: SITE,
 	base: BASE_PATH_WITHOUT_DOMAIN,
-	integrations: [sitemap(), mdx(), lit(), icon()],
+	integrations: [sitemap(), mdx(), icon()],
 });
