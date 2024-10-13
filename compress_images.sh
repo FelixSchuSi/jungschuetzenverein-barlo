733a5064ef7fb422dc82dda04b27f34813e01030
Felix Schulze Sindern <47390169+FelixSchuSi@users.noreply.github.com>
2024-10-13 11:47:33 +0200

    Update compress_images.sh

--- compress_images.sh
+++ compress_images.sh
@@ -1,7 +1,5 @@
 # small version of images
-`for file in ./*; do /mnt/c/Users/fschu/Downloads/libwebp-1.4.0-windows-x64/libwebp-1.4.0-window
-s-x64/bin/cwebp.exe -q 10 "$file" -o "webp_klein/${file%.*}.webp"; done`
+`for file in ./*; do /mnt/c/Users/fschu/Downloads/libwebp-1.4.0-windows-x64/libwebp-1.4.0-windows-x64/bin/cwebp.exe -q 10 "$file" -o "webp_klein/${file%.*}.webp"; done`
 
 # big version of images
-`for file in ./*; do /mnt/c/Users/fschu/Downloads/libwebp-1.4.0-windows-x64/libwebp-1.4.0-window
-s-x64/bin/cwebp.exe -q 75 "$file" -o "webp_gross/${file%.*}.webp"; done`
+`for file in ./*; do /mnt/c/Users/fschu/Downloads/libwebp-1.4.0-windows-x64/libwebp-1.4.0-windows-x64/bin/cwebp.exe -q 75 "$file" -o "webp_gross/${file%.*}.webp"; done`
